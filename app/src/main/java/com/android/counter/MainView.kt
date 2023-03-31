db89ea80fc7107eb0b7384eeb39759878779c8e0
katerina <skirnevskaya00@gmail.com>
2023-03-31 19:15:26 +0700

    deleted R.id

--- app/src/main/java/com/android/counter/MainView.kt
+++ app/src/main/java/com/android/counter/MainView.kt
@@ -1,5 +1,7 @@
 package com.android.counter
 
 interface MainView {
-    fun setButtonText(index: Int, text: String)
+    fun setButtonOneText(text: String)
+    fun setButtonTwoText(text: String)
+    fun setButtonThreeText(text: String)
 }
