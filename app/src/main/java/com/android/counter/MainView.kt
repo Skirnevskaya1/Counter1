b368b0ef93406654eaf69c295b0b30bae8d9dda6
Ekaterina <57950489+Skirnevskaya1@users.noreply.github.com>
2023-04-06 20:55:05 +0700

    Delete MainView.kt

--- app/src/main/java/com/android/counter/MainView.kt
+++
@@ -1,9 +0,0 @@
-package com.android.counter
-
-//@AddToEndSingle - есть ещё такой алиас
-//@StateStrategyType(AddToEndSingleStrategy::class)
-interface MainView: MvpView {
-    fun setButtonOneText(text: String)
-    fun setButtonTwoText(text: String)
-    fun setButtonThreeText(text: String)
-}
